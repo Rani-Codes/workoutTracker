bd04f17b8e518baa9f73f80f9d393fa886e8038e
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-07-20 23:49:27 -0700

    import initial func from internal to main

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"github.com/Rani-Codes/workoutTracker/internal/app"
+)
 
+func main() {
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+
+	app.Logger.Println("App is running")
 }
