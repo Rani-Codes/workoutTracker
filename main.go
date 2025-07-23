cc13e8de58f239a38f3bf50306ed6c0cc9b9bce5
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-07-22 17:16:42 -0700

    create an http server and health check to verify status

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/Rani-Codes/workoutTracker/internal/app"
 )
 
@@ -11,4 +15,21 @@ func main() {
 	}
 
 	app.Logger.Println("App is running")
+
+	http.HandleFunc("/health", HealthCheck)
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Status is available\n")
 }
