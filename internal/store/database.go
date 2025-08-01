3a932decdbe602e2439356e8a37e3961dc01099d
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-08-01 00:00:25 -0700

    created docker compose file for db

---
+++ internal/store/database.go
@@ -0,0 +1,31 @@
+package store
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+func Open() (*sql.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("db: open %w", err)
+	}
+
+	fmt.Println("Connected to database...")
+
+	return db, nil
+}
